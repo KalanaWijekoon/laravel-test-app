a026796daaf593bf416d7595b5d3dc2d9d35b0dd
kalana_wijekoon <raptorcriativosl@gmail.com>
2023-03-05 05:52:37 +0000

    Create user model and routes
    Update User model, migration to remove unnecessary columns add new coulumns.
    Create controller with resources.
    Create resource route.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('country')->nullable();
+            $table->float('rate', 5, 2);
+            $table->string('currency');
             $table->timestamps();
         });
     }
